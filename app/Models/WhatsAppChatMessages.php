318b9d9584720b03e5e75cbc4ef55c330fb802bb
ipanshar <63735756+ipanshar@users.noreply.github.com>
2025-11-13 14:33:00 +0500

    Добавлены новые поля в заполняемые атрибуты модели WhatsAppChatMessages

--- app/Models/WhatsAppChatMessages.php
+++ app/Models/WhatsAppChatMessages.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class WhatsAppChatMessages extends Model
 {
-    protected $fillable = ['chat_list_id', 'message', 'message_id', 'type', 'user_id'];
+    protected $fillable = ['chat_list_id', 'message', 'message_id', 'type', 'user_id', 'status', 'error_code', 'error_message', 'has_response', 'response_to_message_id', 'direction'];
 }
